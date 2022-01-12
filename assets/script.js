becc6b1d1d48a9bff48344d3e60b6407d5e484b9
Courtney Clark <cameron.clark1110@gmail.com>
2022-01-12 13:37:28 -0500

    basic fetch and some initial variables

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,9 @@
-var requestURL = 'https://cors-anywhere.herokuapp.com/https://openfarm.cc/api/v1/crops/?filter=tomato';
+var requestURL = '';
+var srchBtn =  $('.searchBt');
+var plantName = $('.searchField').val();
+var plantData = [];
+
+
 
 
 function getApi(url) {
@@ -15,7 +20,35 @@ function getApi(url) {
       .then(function (data) {
         // Make sure to look at the response in the console and read how 404 response is structured.
         console.log(data);
+        plantData = data[0];
+        console.log(plantData);
       });
   }
 
- getApi(requestURL);
+ 
+
+ function plantSearch () {
+   //e.preventDefault();
+   plantName = 'pepper';//$('.searchField').val();
+   requestURL = 'https://cors-anywhere.herokuapp.com/https://openfarm.cc/api/v1/crops/?filter='+plantName;
+   getApi(requestURL);
+ }
+
+plantSearch();
+ //srchBtn.on('click', plantSearch);
+
+ /*data we want: 
+ 
+ name:
+ scientific name:
+ img:
+ description:
+ sun req:
+ spacing:
+ sowing method
+
+
+ 
+ 
+ 
+ */
