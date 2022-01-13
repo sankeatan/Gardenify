7fde45537b717b804e094448d0ed4135496da687
William Gartman <wngartman@gmail.com>
2022-01-13 11:49:07 -0600

    Added hardiness zone search

--- assets/script.js
+++ assets/script.js
@@ -8,6 +8,7 @@ var plantDesc = "";
 $('#sci-name').hide();
 $('#plant-name').hide();
 
+
 function getApi(url) {
     fetch(url)
       .then(function (response) {
@@ -50,6 +51,31 @@ function getApi(url) {
  }
 
 plantSearch();
+const settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://cors-anywhere.herokuapp.com/https://plant-hardiness-zone.p.rapidapi.com/zipcodes/" + 76904,
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "plant-hardiness-zone.p.rapidapi.com",
+		"x-rapidapi-key": "b71a1c4a5bmshb848c727310c6bbp18da7cjsnbb90f586f1b4"
+	}
+};
+
+
+hardiSearch()
+
+
+function hardiSearch() {
+
+  var urlHard = settings.url
+  inputUrl = urlHard + 76904
+  console.log(inputUrl)
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+  });
+}
+
  //srchBtn.on('click', plantSearch);
 
  /*data we want: 
